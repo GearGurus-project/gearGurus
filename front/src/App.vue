9d46ba13904a3e25fa4daabc414f1390b8963c5b
Joseph <joseph.renno@hotmail.com>
2024-05-18 11:42:56 +0200

    NavBar front

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Navbar />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
-
 <script>
+import Navbar from './components/Nav-Bar.vue'
 import HelloWorld from './components/HelloWorld-Page.vue'
 
 export default {
   name: 'App',
   components: {
+    Navbar,
     HelloWorld
   }
 }
@@ -16,11 +20,11 @@ export default {
 
 <style>
 #app {
+  background-color: #F9E8C9;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- front/src/components/Nav-Bar.vue
+++ front/src/components/Nav-Bar.vue
@@ -1,27 +1,64 @@
 <template>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Navbar</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="/">Home</b-nav-item>
-          <b-nav-item href="/about">About</b-nav-item>
-          <b-nav-item href="/contact">Contact</b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="/login">Login</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Nav-Bar'
-  };
-  </script>
-  
-  <style scoped>
-  /* Add any custom styles here */
-  </style>
-  
+  <nav class="navbar navbar-expand-md navbar-dark navbar-custom">
+    <router-link to="/" class="navbar-brand mr-auto">
+      <img src="..\assets\Efrei-Logo.png" alt="Logo">
+    </router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Accueil</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/emprunts" class="nav-link">Emprunts</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/materiels" class="nav-link">Materiels</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/statistiques" class="nav-link">Statistiques</router-link>
+        </li>
+      </ul>
+    </div>
+    <router-link to="/" class="navbar-disconnect ml-auto">
+      <button class="btn disconnect-btn" @click="disconnect">
+        <img src="..\assets\LogOut.png" alt="Disconnect">
+      </button>
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    disconnect() {
+      // Handle disconnect logic here
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbar-custom {
+  background-color: #201658;
+}
+
+.navbar-brand img {
+  max-height: 40px;
+}
+
+.navbar-nav .nav-link {
+  color: #fff;
+  font-weight: bold;
+}
+
+.disconnect-btn {
+  color: #fff;
+}
+
+.navbar-disconnect img {
+  max-height: 40px;
+}
+</style>

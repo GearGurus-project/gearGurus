8a5511d4233208a853922c2140c98d541b30270c
Samy HAOUARI <haouarisamy@gmail.com>
2024-05-21 21:45:15 +0200

    Connecting with Google database + adding services :)

--- back/src/main/java/net/backendApplication/Entities/Hardware.java
+++ back/src/main/java/net/backendApplication/Entities/Hardware.java
@@ -13,16 +13,28 @@ public class Hardware {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Setter
+    @Getter
     @Column(name="barcode")
     private String barCode;
+    @Getter
+    @Setter
     @Column(name="model")
     private String model;
+    @Getter
+    @Setter
     @Column(name="datePurchase")
     private String datePurchase;
+    @Getter
+    @Setter
     @Column(name="price")
     private String price;
+    @Getter
+    @Setter
     @Column(name="provider")
     private String provider;
+    @Getter
+    @Setter
     @Column(name="category")
     private String category;
     public void setId(Long id) {
@@ -31,4 +43,5 @@ public class Hardware {
     public Long getId() {
         return id;
     }
+
 }
--- back/src/main/java/net/backendApplication/Services/HardwareServices.java
+++ back/src/main/java/net/backendApplication/Services/HardwareServices.java
@@ -1,2 +1,28 @@
-package net.backendApplication.Services;public class HardwareServices {
+package net.backendApplication.Services;
+
+import net.backendApplication.Entities.Hardware;
+import net.backendApplication.Entities.User;
+import net.backendApplication.Repository.HardwareRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HardwareServices {
+    @Autowired
+    private HardwareRepository hardwareRepository;
+    public List<Hardware> getAllHardwares() {
+        return hardwareRepository.findAll();
+    }
+    public Hardware getHardwareById(Long id) {
+        return hardwareRepository.findById(id).orElse(null);
+    }
+    public Hardware saveHardware(Hardware Hardware) {
+        return hardwareRepository.save(Hardware);
+    }
+    public void deleteHardware(Long id) {
+        hardwareRepository.deleteById(id);
+    }
+
 }
